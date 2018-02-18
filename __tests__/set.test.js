0bdaa8b7cb0e187102ff52b1b5d62c4889bc9f0a
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-18 15:58:35 -0800

    Test: Check for pre-populating a new Set

--- __tests__/set.test.js
+++ __tests__/set.test.js
@@ -8,3 +8,8 @@ test('default new CustomSet is empty', () => {
     const newSet = new CustomSet
     expect(newSet.values).toEqual([])
 })
+
+test('can pre-populate a new Set', () => {
+    const newSet = new CustomSet([1,2])
+    expect(newSet.values).toEqual([1,2])
+})
