8fd08a961908ec18fd0039f7d5aa153c63bf3cd9
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-18 21:04:27 -0800

    Test: Check for difference betwen Sets

--- __tests__/set.test.js
+++ __tests__/set.test.js
@@ -128,3 +128,9 @@ test('can intersect Sets', () => {
     const newSet2 = new CustomSet([3,4,5])
     expect(newSet1.intersect(newSet2)).toEqual([3,4])
 })
+
+test('can check difference between Sets', () => {
+    const newSet1 = new CustomSet([1,2,3,4])
+    const newSet2 = new CustomSet([3,4,5])
+    expect(newSet1.difference(newSet2)).toEqual([1,2])
+})
