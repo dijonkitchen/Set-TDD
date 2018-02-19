33a63558f2b01ba26e59b2732ad4ffec42df1672
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-18 16:25:37 -0800

    Test: Check for adding values to a Set

--- __tests__/set.test.js
+++ __tests__/set.test.js
@@ -18,3 +18,27 @@ test("doesn't pre-populate with duplicates", () => {
     const newSet = new CustomSet([1,1])
     expect(newSet.values).toEqual([1])
 })
+
+test('add values to a Set', () => {
+    const newSet = new CustomSet
+    newSet.add([1])
+    expect(newSet.values).toEqual([1])
+})
+
+test('add values to a Set', () => {
+    const newSet = new CustomSet
+    newSet.add([1,2,3])
+    expect(newSet.values).toEqual([1, 2, 3])
+})
+
+test('add values to a Set', () => {
+    const newSet = new CustomSet
+    newSet.add([1,1,3])
+    expect(newSet.values).toEqual([1,3])
+})
+
+test('add values to a Set', () => {
+    const newSet = new CustomSet([1,2,3])
+    newSet.add([1,1,3])
+    expect(newSet.values).toEqual([1,2,3])
+})
